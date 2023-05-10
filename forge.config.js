b79e007b13e3f93a2f5fecbecb974c5bdb42cfb2
荣顶 <wangrongding@qq.com>
2023-05-10 13:53:42 +0800

    chore: 修改 Electron 打包配置

--- forge.config.js
+++ forge.config.js
@@ -6,14 +6,14 @@ module.exports = {
       name: '@electron-forge/maker-zip',
       platforms: ['darwin'],
     },
-    // {
-    //   name: '@electron-forge/maker-dmg',
-    //   config: {
-    //     name: '壁纸盒子', //可以是中文，但只是安装包名称。
-    //     // icon: './icon.icns',
-    //     // background: './share_parent_bg_blue.png',
-    //   },
-    // },
+    {
+      name: '@electron-forge/maker-dmg',
+      config: {
+        name: 'wallpaper-box', //可以是中文，但只是安装包名称。
+        // icon: './icon.icns',
+        // background: './share_parent_bg_blue.png',
+      },
+    },
     // ======================
     // {
     //   name: '@electron-forge/maker-deb',
