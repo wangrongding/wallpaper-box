c684a074022597b086a5da2e8559601e544f4b64
荣顶 <wangrongding@qq.com>
2023-05-15 23:06:21 +0800

    fix: 修复本地壁纸文件夹不存在时的页面异常问题

--- src/pages/ListLocal/index.tsx
+++ src/pages/ListLocal/index.tsx
@@ -1,5 +1,4 @@
-import { Image as AntImage } from 'antd'
-import { CheckCircleFilled, EyeFilled, DeleteFilled } from '@ant-design/icons'
+import { Empty, message } from 'antd'
 import { ipcRenderer } from 'electron'
 import { Image as CusImage } from '@/components/Image'
 
@@ -7,17 +6,21 @@ const fs = require('fs')
 const path = require('path')
 const os = require('os')
 const dir = path.join(os.homedir(), '/wallpaper-box')
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir)
+}
 
 export default function List() {
   const [wallpaperList, setWallpaperList] = useState<string[]>([])
   const [visible, setVisible] = useState(-1)
+  const [messageApi, contextHolder] = message.useMessage()
   // 获取壁纸
   let mounted = false
   // 获取 /wallpaper-box 文件夹中的所有壁纸
   function getWallpaperList() {
     // 对文件进行过滤, 只保留图片
     let files: string[] = []
-    fs.readdirSync(dir).map((file: string) => {
+    fs.readdirSync(dir).forEach((file: string) => {
       if (['.jpg', '.png', '.jpeg'].includes(path.extname(file))) {
         files.push(path.join(dir, file))
       }
@@ -31,6 +34,17 @@ export default function List() {
     getWallpaperList()
   }
 
+  // 设置壁纸
+  const setAsBackground = async (item: string) => {
+    // 设置壁纸
+    ipcRenderer.send('set-wallpaper', item)
+    // 通知主进程关闭动态壁纸
+    ipcRenderer.send('close-live-wallpaper')
+    // 通知主进程设置壁纸完成 (系统弹窗通知)
+    // ipcRenderer.send('asynchronous-message', '设置成功！')
+    messageApi.success('设置成功！')
+  }
+
   useEffect(() => {
     getWallpaperList()
 
@@ -41,24 +55,29 @@ export default function List() {
 
   return (
     <div className='list-page'>
-      <div className='' style={{ columnCount: 5, columnGap: '6px' }}>
-        {wallpaperList.map((item: any, index: number) => {
-          return (
-            <CusImage
-              key={index}
-              src={`file://${item}`}
-              previewSrc={`file://${item}`}
-              visible={visible}
-              index={index}
-              onPreview={() => setVisible(index)}
-              onSet={() => ipcRenderer.send('set-wallpaper', item)}
-              onVisibleChange={() => setVisible(-1)}
-              onDelete={() => deleteWallpaper(item)}
-              style={{ breakInside: 'avoid-column', marginBottom: '6px' }}
-            />
-          )
-        })}
-      </div>
+      {contextHolder}
+      {wallpaperList.length > 0 ? (
+        <div className='' style={{ columnCount: 5, columnGap: '6px' }}>
+          {wallpaperList.map((item: string, index: number) => {
+            return (
+              <CusImage
+                key={index}
+                src={`file://${item}`}
+                previewSrc={`file://${item}`}
+                visible={visible}
+                index={index}
+                onPreview={() => setVisible(index)}
+                onSet={() => setAsBackground(item)}
+                onVisibleChange={() => setVisible(-1)}
+                onDelete={() => deleteWallpaper(item)}
+                style={{ breakInside: 'avoid-column', marginBottom: '6px' }}
+              />
+            )
+          })}
+        </div>
+      ) : (
+        <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+      )}
     </div>
   )
 }
