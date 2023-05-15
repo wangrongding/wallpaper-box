a099a1e399797d4bc56c5e08fcecbfe00ad1cdf5
荣顶 <wangrongding@qq.com>
2023-05-15 23:08:04 +0800

    feat: 完善网络代理功能,开机自启功能,添加 ping 功能

--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -6,7 +6,9 @@ const store = new Store()
 export default function Setting() {
   const [rootPath, setRootPath] = useState('/wallpaper-box')
   const [proxyPath, setProxyPath] = useState('')
+  const [autoLaunch, setAutoLaunch] = useState(false)
   const [messageApi, contextHolder] = message.useMessage()
+  const [loading, setLoading] = useState(false)
 
   // 设置成功回调
   function handleSetSuccess() {
@@ -17,6 +19,8 @@ export default function Setting() {
   function setAutoStart(val: boolean) {
     ipcRenderer.send('set-auto-launch', val)
     handleSetSuccess()
+    setAutoLaunch(val)
+    store.set('auto-launch', val)
   }
 
   // 设置网络代理
@@ -26,8 +30,33 @@ export default function Setting() {
     handleSetSuccess()
   }
 
+  // 测试网络代理
+  async function ping() {
+    store.set('proxy-path', proxyPath)
+    ipcRenderer.send('set-proxy', proxyPath)
+    setLoading(true)
+    // 测试访问 https://www.google.com
+    try {
+      const res = await fetch('https://www.google.com')
+      if (res.status === 200) {
+        messageApi.success('访问 Google 通了！！！')
+      } else {
+        messageApi.error('访问 Google失败。。。', res.status)
+      }
+    } catch {
+      messageApi.error('请求不通，请检查代理是否正确！')
+    } finally {
+      setLoading(false)
+    }
+  }
+
   useEffect(() => {
+    // 读取代理地址
     setProxyPath(store.get('proxy-path'))
+    // 读取图片存储位置
+    setRootPath(store.get('root-path'))
+    // 读取开机自启
+    setAutoLaunch(store.get('auto-launch'))
     return () => {}
   }, [])
 
@@ -42,7 +71,7 @@ export default function Setting() {
       <div className=' rounded-lg border-slate-400 border-4 my-4 p-8'>
         <div>
           <label htmlFor='auto-start'>开机自启：</label>
-          <Switch id='auto-start' size='default' checkedChildren='开启' unCheckedChildren='关闭' onChange={setAutoStart} />
+          <Switch id='auto-start' size='default' checked={autoLaunch} checkedChildren='开启' unCheckedChildren='关闭' onChange={setAutoStart} />
         </div>
 
         <div className='flex items-center'>
@@ -58,20 +87,26 @@ export default function Setting() {
       </div>
 
       <div className=' rounded-lg border-slate-400 border-4 my-4 p-8'>
-        <h2>网络代理：( 例: http://localhost:7890 )</h2>
-        <label htmlFor='proxy'>代理服务器地址：</label>
-        <Input
-          id='proxy'
-          value={proxyPath}
-          type='text'
-          onChange={(val) => {
-            setProxyPath(val.target.value)
-          }}
-          style={{ width: '300px' }}
-        />
-        <Button type='primary' className='ml-4' onClick={setProxy}>
-          修改
-        </Button>
+        <h2>网络代理：(HTTP_PROXY)</h2>
+        <div className='flex items-center'>
+          <label htmlFor='proxy'>代理服务器地址：</label>
+          <Input
+            id='proxy'
+            value={proxyPath}
+            placeholder='例: http://localhost:7890'
+            type='text'
+            onChange={(val) => {
+              setProxyPath(val.target.value)
+            }}
+            style={{ width: '200px' }}
+          />
+          <Button type='primary' className='ml-4' onClick={setProxy}>
+            修改
+          </Button>
+          <Button type='default' style={{ background: '#34d399', color: 'white' }} loading={loading} className='ml-4' onClick={ping}>
+            测试
+          </Button>
+        </div>
       </div>
     </div>
   )
